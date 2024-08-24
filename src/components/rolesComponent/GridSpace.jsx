2ff4d10a96b5b99ae109d3e8637186b4cb4d7153
VishuKalier2003 <vishukalier123@gmail.com>
2024-08-25 00:19:15 +0530

    GridSpace section updateII

--- src/components/rolesComponent/GridSpace.jsx
+++ src/components/rolesComponent/GridSpace.jsx
@@ -19,13 +19,13 @@ const GridSpace = () => {
         onMouseEnter={() => {enterEvent()}} onMouseLeave={() => {leaveEvent()}}>
             <motion.div className="relative w-auto h-full flex flex-row justify-center items-center change-bg1 rounded-3xl"
             initial={{left : '-100vw'}} animate={inside ? {left : '0px'} : {left : '-100vw'}}
-            transition={{type : 'tween', ease : 'easeInOut', duration : 0.5}}>
+            transition={{type : 'tween', ease : 'easeInOut', duration : 1}}>
                 <p className="absolute text-[2rem] md:text-5xl lg:text-[6rem] faded-white text-center">AI Club</p>
                 <p className="absolute text-[2rem] md:text-5xl lg:text-[4rem] light-blue text-center">Core Member</p>
             </motion.div>
             <motion.div className="relative w-auto h-full flex flex-row justify-center items-center change-bg1 rounded-3xl"
             initial={{left : '100vw'}} animate={inside ? {left : '0px'} : {left : '100vw'}}
-            transition={{type : 'tween', ease : 'easeInOut', duration : 0.5}}>
+            transition={{type : 'tween', ease : 'easeInOut', duration : 1}}>
                 <p className="absolute text-[2rem] md:text-5xl lg:text-[6rem] text-center faded-white">Blockchain Club</p>
                 <p className="absolute text-[2rem] md:text-5xl lg:text-[4rem] text-center light-blue">R&D Head</p>
             </motion.div>
